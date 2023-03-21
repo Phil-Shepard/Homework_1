47f440223c111cc459aba71b7d8a4f56f2866564
Phil-Shepard <Philipp-74@yandex.ru>
2023-03-21 21:03:34 +0500

    кое-что исправил

--- src/main/java/ru/Shamonin/Homework_1/Computers/ComputersApplication.java
+++ src/main/java/ru/Shamonin/Homework_1/Computers/ComputersApplication.java
@@ -2,7 +2,7 @@ package ru.Shamonin.Homework_1.Computers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.Shamonin.Homework_1.gddg.kkk;
+
 
 @SpringBootApplication
 public class ComputersApplication {
